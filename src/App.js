f968b03b9246f099d2889bd0897d85d2ef313d5f
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-05-18 14:57:40 +0100

    Made counter function with onClick event handler

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-  
+
   return (
     <div className="App">
-      <button>-</button>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
       <h1>{counter}</h1>
-      <button>+</button>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }

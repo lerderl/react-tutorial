21e2c23a13ec1d05f6492ae0e212ddc4c75d5a78
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-05-18 14:20:59 +0100

    Ternary operator example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 
 const App = () => {
-  const name = 'Goodnews';
-  const isNameShowing = true;
+  const name = 'Ozioma';
 
   return (
     <div className="App">
-      <h1>Hello, {isNameShowing ? name : 'stranger'}!</h1>
+      <h1>Hello</h1>
+      {name ? (
+        <>
+          <h1>{name}!</h1>
+        </>
+      ) : (
+        <>
+          <h2>test</h2>
+          <h3>There is no name</h3>
+        </>
+      )}
     </div>
   );
 }
